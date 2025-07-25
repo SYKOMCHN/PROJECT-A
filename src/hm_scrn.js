63c7cc80b35607fab970a9c5c6b8959a5f8dcc41
Oscar <highspeedtumbleweed@gmail.com>
2025-07-25 14:56:48 -0400

    included basic information

---
+++ src/hm_scrn.js
@@ -0,0 +1,9 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    background(200, 200, 200);
+    textSize(32);
+    textAlign(CENTER, CENTER);
+    text('Cinema', width / 2, height / 2);
+}
+
+function draw(){}
