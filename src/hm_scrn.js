ca6bf86153daf2f84b5f6fa369aa766fdd3eed33
bvincDev <brvincini@gmail.com>
2025-07-27 16:43:51 -0400

    4:3 aspect ratio is maintained on canvas resize

--- src/hm_scrn.js
+++ src/hm_scrn.js
@@ -1,9 +1,21 @@
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    background(200, 200, 200);
+    // calculate 4:3 aspect ratio that fits within the window
+    let w = windowWidth,
+        h = w * 3 / 4;
+    if (h > windowHeight) {
+        h = windowHeight;
+        w = h * 4 / 3;
+    }
+    createCanvas(w, h).position((windowWidth - w) / 2, (windowHeight - h) / 2);
+    background(200);
     textSize(32);
     textAlign(CENTER, CENTER);
     text('Cinema', width / 2, height / 2);
+}
+
+
+function windowResized() {
+    setup(); // recalculate canvas when screen resizes
 }
 
 function draw(){}
