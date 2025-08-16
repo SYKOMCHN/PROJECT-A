d48e87183a549990ec156ee95526218458b2f1d8
Oscar <highspeedtumbleweed@gmail.com>
2025-08-16 14:29:34 -0400

    implement the settings popup

--- src/hm_scrn.js
+++ src/hm_scrn.js
@@ -1,8 +1,7 @@
 let baseWidth = 800;
 let baseHeight = 600;
 let canvasWidth, canvasHeight;
-let scaleSize;
-let settingPagePop = true;
+let settingPagePop = false;
 let backgroundImage;
 
 function preload(){
@@ -19,9 +18,13 @@ function draw(){ //functions and designs will be placed here
     background(255);
     image(backgroundImage, 0 , 0, canvasWidth, canvasHeight);
 
-    if (settingPagePop == false){
+    push();
+
+    if (settingPagePop == true){
     settingsPage();
     }
+
+    pop();
 }
 
 function canvasFourThreeRatio(){
@@ -39,29 +42,78 @@ function canvasFourThreeRatio(){
 
 
 
-function settingsPage(){
-   
-    fill(100);
-    rect(10, 10, canvasWidth - 20, canvasHeight - 20);
-    fill(0);
-    textSize(32);
-    textAlign(CENTER, CENTER);
-    text('ManCine', width / 2, height / 2);
+function settingsPage(){ //modify the scaling.
+    let settingButtonMargin = canvasWidth / 20 ; //*0.05
+    let settingBoxX = settingButtonMargin;
+    let settingBoxY = settingButtonMargin;
+    let settingBoxW = canvasWidth - 2 * settingButtonMargin;
+    let settingBoxH = canvasHeight - 2 * settingButtonMargin;
 
-    scaleSize = canvasWidth / baseWidth;
-    push(); //not sure yet
-    scale(scaleSize);
-    fill(200);
-    rect(20, 20, 200 , 200);
-    textSize(12);
+    fill(255);
+    rect(settingBoxX, settingBoxY, settingBoxW, settingBoxH, canvasWidth / 50); //*0.02
+
     fill(0);
-    textAlign(CENTER, CENTER);
-    text('Hello here', 200 / 2 , 200 / 2);
-    pop();
+    textSize(canvasHeight / 15);
+    textAlign(CENTER,CENTER);
+    text('ManCine', canvasWidth / 2, canvasHeight / 2);
+
+    closeXButton();
+
+    mousePressed();
+
+    
+}
+
+// standalone files in the future
+function infosPage(){}
+function levelsPage(){}
+
+
+
+
+
+
+function closeXButton(){
+
+    let closeBtnX = canvasWidth / 1.15; //*0115 
+    let closeBtnY = canvasWidth / 16;
+    let closeBtnW = canvasWidth / 15;
+    let closeBtnH = canvasHeight / 15;
+    
+    fill('red');
+    rect(closeBtnX,closeBtnY,closeBtnW,closeBtnH, canvasWidth / 75);
+    return {closeBtnX,closeBtnY,closeBtnW,closeBtnH}; //makes these global
+
+    
+}
+
+function settingButton(){
+    settingPagePop = true;
+}
+
+function mousePressed(){
+
+    if(settingPagePop == true){
+
+        let { closeBtnX,closeBtnY,closeBtnW,closeBtnH} = closeXButton();
+
+        if(
+            mouseX > closeBtnX &&
+            mouseX < closeBtnX + closeBtnW &&
+            mouseY > closeBtnY &&
+            mouseY < closeBtnY + closeBtnH 
+            //&& mouse pressed function
+        ) 
+        {
+            settingPagePop = false;
+        }
+    }
+
+    
 }
 
 function windowResized(){
     canvasFourThreeRatio();
 
-    settingsPage();
+   //settingsPage(); //optional?
 }
